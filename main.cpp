3fe58a3e474e9b682776faac17549bea757e66e3
Nick <steichennicholas@gmail.com>
2019-08-29 08:30:37 -0700

    Added comments
    Added a few comments for explanation.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Hello world :D");
 
+	// Bound between -.95 - 0.95 as to give some distance at the edges.
 	TravelingSalesman test = TravelingSalesman(5000, -0.95, 0.95);
 	test.solveNN();
 	test.printStats(true);
 	//test.printPath();
 	std::cout << std::endl << std::endl << std::endl << std::endl;
+	
+	// Holds the program open for the user to view the final render.
 	int x;
 	std::cin >> x;
 
